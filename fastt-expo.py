bd1381ec7bf1e5d76c9dbb9897d622ffca65daf9
iamishan9 <iamishan9@gmail.com>
2020-11-02 15:44:27 +0100

    Update fast-expo.py

--- fastt-expo.py
+++ fastt-expo.py
@@ -8,3 +8,18 @@ def fast_exp(X, e):
     return fast_exp(X,e-1)*X
 
 print(fast_exp(2,5))
+
+
+
+def pow_h(base, degree, module):
+    degree = bin(degree)[2:]
+    r = 1
+    for i in range(len(degree) - 1, -1, -1):
+        r = (r * base ** int(degree[i])) % module
+        base = (base ** 2) % module
+    return r
+
+#test
+print(pow_h(3,4,100))
+# for i in range(16):
+#     print(i, 2**i, pow_h(2, i, 100))
