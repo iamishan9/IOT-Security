301fe004db0f132a97f2aaa6959696a36246dca5
joshuashakya <joshuashakya96@gmail.com>
2020-11-13 20:48:45 +0100

    LCG XORshift comparison

--- random_generator_test.py
+++ random_generator_test.py
@@ -1,5 +1,6 @@
 from random_generators import lcg,glsfr,mersenne_twister as mt, bbs, isaac, xorshift as xs
 import time
+import timeit
 
 class Seed(object):
     # Function that generates seed
@@ -13,6 +14,7 @@ class TimeSeed(Seed):
     def generate_seed(self):
         return time.time()
 
+lcg_time,xorshift_time = 0,0
 print("Generating 10 random numbers using Linear Congruetional Generator (LCG):")
 for i in range(10): 
     time.sleep(0.01)
@@ -26,13 +28,18 @@ for i in range(10):
         random_number = random_number.split('.')[1]
     # Do not split if no decimal point and just take the integer as it is
     random_number = int(random_number)
+    start_time =timeit.default_timer()
     key = lcg.LCG(random_number, 1)[0]
+    lcg_time=timeit.default_timer()-start_time
     print(key)
 
 print("\nGenerating 10 random numbers using XORshift:")
 # mt_rn = mt.mersenne_rng(1131464071)
 for i in range(10):
-    print(xs.xor_shift(1,2**20,TimeSeed()))
+    start_time = timeit.default_timer()
+    xs_rn=xs.xor_shift(1,2**20,TimeSeed())
+    xorshift_time=timeit.default_timer()-start_time
+    print(xs_rn)
 
 print("\nGenerating 10 random numbers using Linear feedback shift registers(LFSR):")
 lfsr_rn = glsfr.LfsrRandom(0b10110100000000001, 1)
@@ -57,4 +64,15 @@ issac_rn = isaac.Isaac()
 for _ in range(0,10):
     number = issac_rn.rand()
     print(number)
+
+print("\nComparing the time required for random generation using LCG and XORshift\n")
+print("Algorithm\tExecution time")
+print("LCG      \t{}".format(lcg_time))
+print("XORshift \t{}".format(xorshift_time))
+
+print("\n")
+if(lcg_time<xorshift_time):
+    print("Hence, it is proved that LCG is faster.\n")
+    
+
 
