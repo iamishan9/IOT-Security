2e1492f15168e8e1b7c4dea14850c4c274968196
iamishan9 <iamishan9@gmail.com>
2020-11-14 21:25:06 +0100

    Add utils file and add color to test file key exchange

--- key_exchange_test.py
+++ key_exchange_test.py
@@ -1,4 +1,5 @@
 from key_exchange import diffie_hellman as dh
+from utils import bcolors
 
 
 message="This is a very secret message!!!"
@@ -11,12 +12,14 @@ j_private=157
 Ishan = dh.DH_Endpoint(i_public, j_public, i_private)
 Joshua = dh.DH_Endpoint(i_public, j_public, j_private)
 
+print(bcolors.OKGREEN+"Partial keys are",bcolors.ENDC)
 i_partial=Ishan.generate_partial_key()
 print(i_partial) #147
 
 j_partial=Joshua.generate_partial_key()
 print(j_partial)
 
+print(bcolors.OKGREEN+"Full keys generated with partial keys are",bcolors.ENDC)
 i_full=Ishan.generate_full_key(j_partial)
 print(i_full) #75
 
@@ -25,7 +28,10 @@ print(j_full)
 
 
 j_encrypted=Joshua.encrypt_message(message)
-print(j_encrypted) #'\x9f³´¾k´¾k¬kÁ°½Äk¾°®½°¿k¸°¾¾¬²°lll'
+print(bcolors.OKGREEN+"Encrypted text is\n"+bcolors.ENDC,j_encrypted)
+# print('\n') #'\x9f³´¾k´¾k¬kÁ°½Äk¾°®½°¿k¸°¾¾¬²°lll'
+
 
 i_decrypted = Ishan.decrypt_message(j_encrypted)
-print(i_decrypted) #'This is a very secret message!!!'
+print(bcolors.OKGREEN+"Decrypted text is\n"+bcolors.ENDC,i_decrypted)
+# print(i_decrypted) #'This is a very secret message!!!'
